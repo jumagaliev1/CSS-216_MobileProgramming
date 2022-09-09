9288d9674abebcc2daaef28bd861aed61a3f6e23
jumagaliev1 <200103311@stu.sdu.edu.kz>
2022-09-09 14:35:32 +0600

    3. Learn more about functions (step 2.1)

--- src/main/kotlin/Hello.kt
+++ src/main/kotlin/Hello.kt
@@ -1,8 +1,23 @@
 import java.util.*
 
+fun fishFood (day : String) : String {
+    var food = ""
+    when (day) {
+        "Monday" -> food = "flakes"
+        "Tuesday" -> food = "pellets"
+        "Wednesday" -> food = "redworms"
+        "Thursday" -> food = "granules"
+        "Friday" -> food = "mosquitoes"
+        "Saturday" -> food = "lettuce"
+        "Sunday" -> food = "plankton"
+    }
+    return food
+}
+
 fun feedTheFish() {
     val day = randomDay()
-    val food = "pellets"
+    val food = fishFood(day)
+
     println ("Today is $day and the fish eat $food")
 }
 
