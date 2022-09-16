3509360235a15d83f16427a2e283985bed425a64
jumagaliev1 <200103311@stu.sdu.edu.kz>
2022-09-16 14:41:03 +0600

    9. Learn about singletons and enums (step 2)

--- src/main/kotlin/example/myapp/decor/Decoration.kt
+++ src/main/kotlin/example/myapp/decor/Decoration.kt
@@ -2,6 +2,9 @@ package example.myapp.decor
 
 data class Decoration2(val rocks: String, val wood: String, val diver: String){
 }
+enum class Color(val rgb: Int) {
+    RED(0xFF0000), GREEN(0x00FF00), BLUE(0x0000FF);
+}
 
 fun makeDecorations() {
     val d5 = Decoration2("crystal", "wood", "diver")
