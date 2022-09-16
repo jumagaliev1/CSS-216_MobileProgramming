5b0f87178d42fd87eb6fb21896c5515693e8922d
jumagaliev1 <200103311@stu.sdu.edu.kz>
2022-09-16 11:02:45 +0600

    5. Learn about subclasses and inheritance (step 2)

--- src/main/kotlin/example/myapp/Aquarium.kt
+++ src/main/kotlin/example/myapp/Aquarium.kt
@@ -1,11 +1,12 @@
 package example.myapp
 
+import java.lang.Math.PI
 open class Aquarium(length: Int = 100, width: Int = 20, height: Int = 40) {
     init {
         println("aquarium initializing")
     }
     var width: Int = 20
-    var height: Int = 40
+    open var height: Int = 40
     var length: Int = 100
 
     fun printSize() {
@@ -26,4 +27,16 @@ open class Aquarium(length: Int = 100, width: Int = 20, height: Int = 40) {
     open val shape = "rectangle"
     open var water: Double = 0.0
         get() = volume * 0.9
+}
+class TowerTank (override var height: Int, var diameter: Int): Aquarium(height = height, width = diameter, length = diameter) {
+    override var volume: Int
+        // ellipse area = π * r1 * r2
+        get() = (width/2 * length/2 * height / 1000 * PI).toInt()
+        set(value) {
+            height = ((value * 1000 / PI) / (width/2 * length/2)).toInt()
+        }
+
+    override var water = volume * 0.8
+    override val shape = "cylinder"
+
 }
--- src/main/kotlin/example/myapp/Main.kt
+++ src/main/kotlin/example/myapp/Main.kt
@@ -1,8 +1,10 @@
 package example.myapp
 
 fun buildAquarium() {
-    val aquarium6 = Aquarium(length = 25, width = 25, height = 40)
-    aquarium6.printSize()
+    val myAquarium = Aquarium(width = 25, length = 25, height = 40)
+    myAquarium.printSize()
+    val myTower = TowerTank(diameter = 25, height = 40)
+    myTower.printSize()
 }
 
 fun main() {
