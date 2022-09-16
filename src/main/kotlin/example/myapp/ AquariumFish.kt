ea949812eb4512b95dc94bcf2233a37bf056c783
jumagaliev1 <200103311@stu.sdu.edu.kz>
2022-09-16 11:09:51 +0600

    6. Compare abstract classes and interfaces (step 2)

--- src/main/kotlin/example/myapp/ AquariumFish.kt
+++ src/main/kotlin/example/myapp/ AquariumFish.kt
@@ -3,10 +3,20 @@ package example.myapp
 abstract class AquariumFish {
     abstract val color: String
 }
-class Shark: AquariumFish() {
+class Shark: AquariumFish(), FishAction {
     override val color = "grey"
+    override fun eat() {
+        println("hunt and eat fish")
+    }
 }
 
-class Plecostomus: AquariumFish() {
+class Plecostomus: AquariumFish(), FishAction {
     override val color = "gold"
+    override fun eat() {
+        println("eat algae")
+    }
+}
+
+    interface FishAction  {
+    fun eat()
 }
--- src/main/kotlin/example/myapp/Main.kt
+++ src/main/kotlin/example/myapp/Main.kt
@@ -10,9 +10,10 @@ fun buildAquarium() {
 fun makeFish() {
     val shark = Shark()
     val pleco = Plecostomus()
-
     println("Shark: ${shark.color}")
+    shark.eat()
     println("Plecostomus: ${pleco.color}")
+    pleco.eat()
 }
 fun main() {
     makeFish()
